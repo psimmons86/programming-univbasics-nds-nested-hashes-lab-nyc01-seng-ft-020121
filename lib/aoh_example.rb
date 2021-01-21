59af84fb0e68b5babbb0e929f91ab7f6290c5b34
patrick simmons <hadroncollides@gmail.com>
2021-01-21 20:59:33 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -41,7 +41,7 @@ end
 
 def aoh_update(aoh, row, key, new_value)
   assembled_aoh[1][:name] = new_value
-  assembled_aoh
+  
 
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
