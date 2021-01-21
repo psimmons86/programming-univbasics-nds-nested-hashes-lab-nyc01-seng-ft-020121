81a2547e75c15a63b8aa931f139850d813a3dd6a
patrick simmons <hadroncollides@gmail.com>
2021-01-21 20:22:28 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -22,8 +22,8 @@ def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
   literal_aoh = [
-    {:name => "Joelle Van Dyne", :occupation => "Radio Personality"},
     {:name => "Don Gately", :occupation => "Live-in Staff"},
+    {:name => "Joelle Van Dyne", :occupation => "Radio Personality"},
     {:name =>  "Pat Monteseian", :occupation => "Staff"},
     {:name => "Kate Gompert", :occupation => "None"},
     {:name =>  "Bruce Green", :occupation => "Fan of Mildred"}
