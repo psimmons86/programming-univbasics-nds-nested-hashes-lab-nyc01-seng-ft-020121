616437a4decf7ad5de027cc8f0090e61bf76388b
patrick simmons <hadroncollides@gmail.com>
2021-01-21 20:30:19 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -40,7 +40,7 @@ def aoh_lookup(aoh, row, key)
 end
 
 def aoh_update(aoh, row, key, new_value)
-  assembled_aoh[1][:name] = 
+  assembled_aoh[1][:name] = "Joelle van Dyne (PGOAT)"
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
