e299f4db0adbcfa10d23e3b9a57f54a169597afe
patrick simmons <hadroncollides@gmail.com>
2021-01-21 20:19:30 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -21,6 +21,10 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
+  literal_aoh = [
+    {:name => "Joelle Van Dyne", :occupation => "Radio Personality"},
+    {:name => "Don Gately", :occupation => "Live-in Staff"}
+    ]
 end
 
 def aoh_lookup(aoh, row, key)
